f8d4dff727d265060ea58670eca2be71cd13aa67
tyrial87 <jonathan.dixon@thepalladiumgroup.com>
2016-09-20 11:50:11 +0200

    Header style and Warning on export
    Re-styled the header and added it to it's own php file to be consistent
    across all pages. Moved the login into the navbar (although it actually
    exists in the header.php). Added an error message when the user tries to
    export the data on Cartographie page without any facility types
    selected.

---
+++ lib/inc/head.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<div id="head">       
+    <a href="/cs/index.php">
+        <div id="logocard"> <img src="images/cardlogo.png" width="51" height="61" alt="Logo - Cartographie sanitaire d'haiti">
+            <h2>CARTOGRAPHIE<br/>
+                SANITAIRE D'HAITI </h2>
+            <p>UNIT&Eacute; D'&Eacute;TUDES ET DE PROGRAMMATION</p>
+        </div>
+    </a>
+    <div id="logosite">
+        <div id="logosite_desc">
+            <h2>R&Eacute;PUBLIQUE D'HAITI</h2>
+            <p>MINIST&Egrave;RE DE LA SANT&Eacute; PUBLIQUE <br>
+                ET DE LA POPULATION</p>
+        </div>
+    </div>    
+    <div class="clearfix"></div>
+    <div id="hdlink"><a href="login.php">Se Connecter</a></div>
+</div>
